47041eb9236105cf2848bb2ff45162300ac3b48d
Juliette Suc <juliette.suc@outlook.fr>
2025-03-11 15:05:32 +0100

    Feat : basic routes to a form page with a tailwindcss form

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,14 +1,10 @@
-import './App.css'
 import Form from '../components/Form'
 
 function SignIn() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-        Sign In
-    </h1>
-    <Form />
+        <Form />
     </>
   )
 }
