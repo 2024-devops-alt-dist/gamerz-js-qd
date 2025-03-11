ebe15e1e379cb869e8229c206a897cf793555e1d
Juliette Suc <juliette.suc@outlook.fr>
2025-03-11 14:28:21 +0100

    Feat : tailwind first install

---
+++ frontend/src/pages/SignIn.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Form from '../components/Form'
+
+function SignIn() {
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+        Sign In
+    </h1>
+    <Form />
+    </>
+  )
+}
+
+export default SignIn
