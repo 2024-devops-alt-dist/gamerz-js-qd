954b38b5987ff54e55e8c5d04c88dbf2fe11528c
QuentinDegli <quentinnicolasdegli@gmail.com>
2025-04-07 16:26:41 +0200

    Configure CORS backend et withCredential: true côté front

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,18 +4,19 @@ import userRoutes from "./routes/userRoutes";
 
 const app: Application = express();
 
-/* const corsOptions = {
-    origin:  'http://localhost:5173', // Remplace par l'URL de ton front
+const corsOptions = {
+    origin:  'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
-    allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
-  }; */
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'], // En-têtes autorisés
+    credentials: true, // Autoriser les cookies
+  }; 
 
 
 const version = "v1";
 const path = `/api/${version}`;
 
 // Middleware pour autoriser les requêtes CORS
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Middleware pour analyser les corps de requêtes JSON
 app.use(express.json());
