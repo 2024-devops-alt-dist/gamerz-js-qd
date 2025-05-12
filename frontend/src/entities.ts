4b4cf26d57033912942668da2fdebf4c7ab818f5
Juliette Suc <juliette.suc@outlook.fr>
2025-05-12 14:55:23 +0200

    Feat : with entitites

--- frontend/src/entities.ts
+++ frontend/src/entities.ts
@@ -12,3 +12,21 @@ export interface User {
     createdAt?: Date;  
     updatedAt?: Date;  
   }
+
+export interface Channel {
+    _id: string; 
+    title: string; 
+    description: string; 
+    image?: { path: string }; 
+    createdAt: string; 
+    updatedAt: string; 
+  }
+    
+export interface Message {
+  _id: string; 
+  text: string; 
+  channel_id: string; 
+  user_id: string; 
+  createdAt: string; 
+  updatedAt: string; 
+}
